5ed54e517e1cc542c410b7fefce8993c567d4886
Eric Krapil <eric.krapil@gmail.com>
2018-12-05 20:16:37 -0800

    Add scripts. Delete TopTen.java

---
+++ Scripts/project4.sh
@@ -0,0 +1,34 @@
+#ssh -p 2222 -l ejkrapil bridges.psc.edu
+#interact -N <# of nodes> -t HH:MM:SS
+#Run this script (Should not be run unless package structure is correct within $SCRATCH/proj4/)
+
+module load hadoop
+start-hadoop.sh
+sleep 10s #Wait a bit for nodes to be ready, this is a hack to prevent previous failures where hadoop had not loaded fully yet
+echo "~DONE LOADING HADOOP"
+cd $SCRATCH/proj4
+hadoop com.sun.tools.javac.Main TopEmployers.java 
+echo "~Compiled TopEmployers.java"
+jar cf TopEmployers.jar TopEmployers*.class
+echo "~Created TopEmployers .jar"
+hadoop com.sun.tools.javac.Main TopJobTypesApproved.java 	
+echo "~Compiled TopEmployers.java"
+jar cf TopJobTypesApproved.jar TopJobTypesApproved*.class
+echo "~Created TopEmployers .jar"
+
+hadoop fs -mkdir -p input
+echo "~Hadoop input directory created."
+sleep 3s
+hadoop fs -put input/h1b_kaggle.csv input
+sleep 3s
+echo "~h1b_kaggle.csv distributed to HDFS"
+
+echo "~Running Top Employers job"
+hadoop jar TopEmployers.jar TopEmployers input/h1b_kaggle.csv TopEmployers_output
+hadoop fs -cat TopEmployers_output/part-r-00000
+hadoop fs -get TopEmployers_output/part-r-00000 output/TopEmployers_Output.txt
+
+echo "~Running Most Common Job Types Certified job"
+hadoop jar TopJobTypesApproved.jar TopJobTypesApproved input/h1b_kaggle.csv TopJobTypesApproved_output
+hadoop fs -cat TopJobTypesApproved_output/part-r-00000
+hadoop fs -get TopJobTypesApproved_output/part-r-00000 output/TopJobTypesApproved_Output.txt
